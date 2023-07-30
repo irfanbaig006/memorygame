e957e23ef08e728826a58161f14cf7d66a11a9e3
Irfan baig <98215853+irfanbaig006@users.noreply.github.com>
2023-07-30 18:06:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Cards from './Components/Cards'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MEMORY GAME</h1>
+      <Cards />
+    </div>
+  );
+}
+
+
+export default App;
